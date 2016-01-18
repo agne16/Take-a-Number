7b8c357f04c4e89f6435a47b26335d4b4f482014
T.J <agne16@up.edu>
2016-01-17 18:58:24 -0800

    test push

--- server/src/main/java/edu/up/projects/engineering/ServerClass.java
+++ server/src/main/java/edu/up/projects/engineering/ServerClass.java
@@ -4,6 +4,6 @@ public class ServerClass
 {
     public static void main(String[] args)
     {
-        System.out.println("All your base do not are belong to us");
+        System.out.println("All your base do nottt are belong to us");
     }
 }
