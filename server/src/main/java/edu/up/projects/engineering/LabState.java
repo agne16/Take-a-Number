fe13a424484345fdd48b6699ab7339143eedb709
T.J <agne16@up.edu>
2016-01-22 17:20:26 -0800

    parseXML now returns a LabState object

--- server/src/main/java/edu/up/projects/engineering/LabState.java
+++ server/src/main/java/edu/up/projects/engineering/LabState.java
@@ -5,16 +5,68 @@ import java.util.HashMap;
 
 public class LabState
 {
+    private String sessionId;
     private String[] classRoster;
     private boolean[][] checkpoints;
     private ArrayList<String> labQueue;
     private HashMap<String, Integer> seatPositions;
 
-    public LabState(String[] initClassRoster, boolean[][] initCheckpoints,
+    public LabState(String initSessionId, String[] initClassRoster, boolean[][] initCheckpoints,
                     ArrayList<String> initLabQueue)
     {
+        sessionId = initSessionId;
         classRoster = initClassRoster;
         checkpoints = initCheckpoints;
         labQueue = initLabQueue;
+    }
+
+    public String getSessionId()
+    {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId)
+    {
+        this.sessionId = sessionId;
+    }
+
+    public String[] getClassRoster()
+    {
+        return classRoster;
+    }
+
+    public void setClassRoster(String[] classRoster)
+    {
+        this.classRoster = classRoster;
+    }
+
+    public boolean[][] getCheckpoints()
+    {
+        return checkpoints;
+    }
+
+    public void setCheckpoints(boolean[][] checkpoints)
+    {
+        this.checkpoints = checkpoints;
+    }
+
+    public ArrayList<String> getLabQueue()
+    {
+        return labQueue;
+    }
+
+    public void setLabQueue(ArrayList<String> labQueue)
+    {
+        this.labQueue = labQueue;
+    }
+
+    public HashMap<String, Integer> getSeatPositions()
+    {
+        return seatPositions;
+    }
+
+    public void setSeatPositions(HashMap<String, Integer> seatPositions)
+    {
+        this.seatPositions = seatPositions;
     }
 }
