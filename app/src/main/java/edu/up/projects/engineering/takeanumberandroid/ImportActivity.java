ba922ae82f47616aefceb32644941b8f839c60e0
sohm16 <sohm16@up.edu>
2016-01-29 15:57:56 -0800

    Attempt at socket connection in importactivity

--- app/src/main/java/edu/up/projects/engineering/takeanumberandroid/ImportActivity.java
+++ app/src/main/java/edu/up/projects/engineering/takeanumberandroid/ImportActivity.java
@@ -10,9 +10,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+// NICK TESTING SOCKET STUFF
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import android.app.Activity;
+import android.widget.EditText;
+import android.widget.TextView;
+
 public class ImportActivity extends AppCompatActivity {
     String sessionID;
     int[] layoutParams = new int[4];
+
+    // NICK
+    EditText textOut;
+    TextView textIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +35,11 @@ public class ImportActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
-
+        // NICK
+        textOut = (EditText)findViewById(R.id.textout);
+        Button buttonSend = (Button)findViewById(R.id.send);
+        textIn = (TextView)findViewById(R.id.textin);
+        buttonSend.setOnClickListener(buttonSendOnClickListener);
 
         Button setupB = (Button) findViewById(R.id.setupButton);
         Button queueB = (Button) findViewById(R.id.queueButton);
@@ -77,4 +93,57 @@ public class ImportActivity extends AppCompatActivity {
         }
     }
 
+    // NICK
+    //
+    //
+    Button.OnClickListener buttonSendOnClickListener = new Button.OnClickListener() {
+        @Override
+        public void onClick(View arg0) {
+            Socket socket = null;
+            DataOutputStream dataOutputStream = null;
+            DataInputStream dataInputStream = null;
+
+            try {
+                socket = new Socket("192.168.127.1", 8888);
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream.writeUTF(textOut.getText().toString());
+                textIn.setText(dataInputStream.readUTF());
+            }
+            catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    }
+                    catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataOutputStream != null) {
+                    try {
+                        dataOutputStream.close();
+                    }
+                    catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataInputStream != null) {
+                    try {
+                        dataInputStream.close();
+                    }
+                    catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    };
 }
